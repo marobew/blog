c2201d4e158425743500c4a3dbcb1dacfb4b06db
marobew <boramm@icloud.com>
2019-01-29 16:08:46 +0900

    add detail and bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def home(request):
@@ -7,3 +7,8 @@ def home(request):
 
     # 쿼리셋과 메소드의 형식
     # 모델, 쿼리셋(object).메소드
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id) # pk = primary key : 객체들의 이름표, 구분자, 데이터의 대표값
+
+    return render(request, 'detail.html', {'blog': blog_detail})
